bfe636fd4807e3f616dd8c0dccbc784c498a461a
siyuan <1848142281@qq.com>
2024-09-22 19:27:37 +0800

    Minheap class done & Queue interface TODO

--- PriorityQueueRewrite/src/MinHeap.java
+++ PriorityQueueRewrite/src/MinHeap.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * MinHeap class
@@ -108,18 +109,21 @@ public class MinHeap<E> {
 
     /**
      * siftUp向上调整，保持平衡
+     * 首先检查是否定义了比较器comparator。
+     * 如果定义了比较器，那么堆中的元素类型可能不是实现了Comparable接口的类，因此需要使用comparator来比较元素的大小
+     * 如果没有定义比较器，那么假设堆中的元素都是实现了Comparable接口的类，可以直接使用元素的compareTo方法来比较大小
      */
     private void siftUp(int index) {
         if (comparator != null) {
-            // 循环从新增节点开始，自底向上比较子节点和父节点大小，如果子节点大于父节点则交换两者的值
-            while (index > 0 && comparator.compare(list.get(parentIndex(index)), list.get(index)) > 0) {
+            // 循环从新增节点开始，自底向上比较子节点和父节点大小，如果父节点大于子节点则交换两者的值
+            while (index > 0 && comparator.compare(list.get(parentIndex(index)), list.get(index)) > 0) { // cpmpare -> A < B 返回小于0
                 E tempElement = list.get(index);
                 list.set(index, list.get(parentIndex(index)));
                 list.set(parentIndex(index), tempElement);
                 index = parentIndex(index);
             }
         } else {
-            // 循环从新增节点开始，自底向上比较子节点和父节点大小，如果子节点大于父节点则交换两者的值
+            // 循环从新增节点开始，自底向上比较子节点和父节点大小，如果父节点大于子节点则交换两者的值
             E tempElement = list.get(index);
             list.set(index, list.get(parentIndex(index)));
             list.set(parentIndex(index), tempElement);
@@ -142,10 +146,10 @@ public class MinHeap<E> {
     }
 
     /**
-     * 将数组转为堆
+     * 将数组转为堆，在O(n)时间复杂度内完成建堆
      */
     private void heapify() {
-        // 找到最后一个非叶子节点，往前便利
+        // 找到最后一个非叶子节点，往前遍历
         for (int i = parentIndex(list.size() - 1); i >= 0; i--) {
             // 对每个非叶子节点执行siftDown,使其范围内保持小顶堆特性
             siftDown(i);
@@ -157,18 +161,18 @@ public class MinHeap<E> {
      */
     private void siftDown(int index) {
         if (comparator != null) {
-            // 如果左节点小于数组大小才进入循环，避免数组越界
+            // 如果左节点(左孩子)小于数组大小才进入循环，避免数组越界
             while (leftChildIndex(index) < list.size()) {
-                // 获取左索引
+                // 获取左孩子索引
                 int cmpIndex = leftChildIndex(index);
 
-                // 获取左或者右子节点中值更小的索引
+                // 获取左或者右子节点中值更小的索引，如果右孩子更小，会把cmpIndex更新为右孩子的索引
                 if (rightChildIndex(index) < list.size() &&
-                        comparator.compare(list.get(leftChildIndex(index)), list.get(rightChildIndex(index))) <= 0) {
+                        comparator.compare(list.get(leftChildIndex(index)), list.get(rightChildIndex(index))) > 0) { // A > B return 1 -> leftChildkey > rightChildkey
                     cmpIndex = rightChildIndex(index);
                 }
 
-                // 如果父节点比子节点小则停止比较，结束循环
+                // 如果父节点比左右孩子中小的子节点小则停止比较，结束循环
                 if (comparator.compare(list.get(index), list.get(cmpIndex)) <= 0) {
                     break;
                 }
@@ -178,7 +182,7 @@ public class MinHeap<E> {
                 list.set(cmpIndex, list.get(index));
                 list.set(index, tmpElement);
 
-                index = cmpIndex;
+                index = cmpIndex; // 向下调整，不满足特性可能向下传递，完成本次循环之后还要检查更新之后是否符合小顶堆性质
             }
         } else {
             // 如果左节点小于数组大小才进入循环，避免数组越界
@@ -207,17 +211,39 @@ public class MinHeap<E> {
         }
     }
 
+    /**
+     * 查看堆顶元素但不移除的方法peek(),堆顶元素存储在index = 0的位置
+     */
+    public E peek() {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
 
+    public static void main(String[] args) {
+        // MinHeap Test Function
+        int n = 100_0000; // 随机生成1000W个数据存储到小顶堆中
+        MinHeap<Integer> heap = new MinHeap<>(n, Comparator.comparingInt(a -> a)); // lambda
 
+        // 往堆中随机存放1000W个元素
+        for (int i = 0; i < n; i++) {
+            heap.add(ThreadLocalRandom.current().nextInt(0, n));
+        }
 
+        int [] arr = new int[n];
 
+        // 进行出队操作，将元素存到数组中
+        for (int i = 0; i < n; i++) {
+            arr[i] = heap.poll();
+        }
 
-
-
-
-
-
-
-
-
+        // 循环遍历，如果前一个元素比后一个元素大，说明小顶堆有问题（小顶堆存储结构是无序的，逻辑结构是根小于左右孩子的完全二叉树）
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] > arr[i]) {
+//                System.out.printf("i : %d,arr[%d] = %d\n", i, arr[i - 1], arr[i]);
+                throw new RuntimeException("err heap");
+            }
+        }
+    }
 }
---
+++ PriorityQueueRewrite/src/Queue.java
@@ -0,0 +1,9 @@
+/**
+ * Use for PriorityQueue
+ *
+ * @author Siyuan
+ * @Date 2024/9/22 19:25
+ */
+public interface Queue<E> {
+
+}
